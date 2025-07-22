c0f9f2a4c838cbe99ee766cbf8a295d83dce2af8
phonkmasti <fartuchoknik1@gmail.com>
2025-07-22 20:52:53 +0200

    Templates users app complete

--- app/settings.py
+++ app/settings.py
@@ -50,6 +50,7 @@ INSTALLED_APPS = [
 
     "main",
     "goods",
+    "users",
 ]
 
 MIDDLEWARE = [
@@ -70,7 +71,7 @@ ROOT_URLCONF = 'app.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [],
+        'DIRS': [BASE_DIR / "templates"],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
