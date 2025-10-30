6fc8b9d62aaa6c5ea54d3c38985c6d35ad1566bb
phonkmasti <fartuchoknik1@gmail.com>
2025-10-30 21:10:48 +0100

    create order is ready!!

--- orders/admin.py
+++ orders/admin.py
@@ -1,3 +1,86 @@
+import re
+import stat
 from django.contrib import admin
 
+from orders.models import Order, OrderItem
+
 # Register your models here.
+
+
+class OrderItemTabulareAdmin(admin.TabularInline):
+    model = OrderItem
+    fields = "product", "name", "price", "quantity"
+    search_fields = (
+        "product",
+        "name",
+    )
+    extra = 0
+
+
+@admin.register(OrderItem)
+class OrderItemAdmin(admin.ModelAdmin):
+    list_display = (
+        "order",
+        "product",
+        "name",
+        "price",
+        "quantity",
+    )
+    list_filter = (
+        "order",
+        "product",
+        "name",
+        "price",
+        "quantity",
+        "created_timestamp",
+    )
+    search_fields = (
+        "order",
+        "product",
+        "name",
+    )
+
+
+class OrderTabulareAdmin(admin.TabularInline):
+
+    model = Order
+
+    fields = (
+        "requires_delivery",
+        "status",
+        "created_timestamp",
+        "is_paid",
+        "payment_on_get",
+    )
+
+    search_fields = (
+        "requires_delivery",
+        "created_timestamp",
+        "is_paid",
+        "payment_on_get",
+    )
+    readonly_fields = ("created_timestamp",)
+    extra = 0
+
+
+@admin.register(Order)
+class OrderAdmin(admin.ModelAdmin):
+    list_display = (
+        "id",
+        "user",
+        "requires_delivery",
+        "status",
+        "created_timestamp",
+        "payment_on_get",
+        "is_paid",
+    )
+
+    search_fields = ("id",)
+    readonly_fields = ("created_timestamp",)
+    list_filter = (
+        "requires_delivery",
+        "payment_on_get",
+        "is_paid",
+        "status",
+    )
+    inlines = (OrderItemTabulareAdmin,)
