b27a44a9c44dc8699b1992bc33767d8e3868b6a4
Phonkmasti <fartuchoknik1@gmail.com>
2025-12-09 20:21:51 +0100

    header base templates done

--- goods/views.py
+++ goods/views.py
@@ -28,7 +28,6 @@ def catalog(request, category_slug=None):
     current_page = paginator.page(int(page))
 
     context = {
-        "title": "Home - Каталог",
         "goods": current_page,
         "slug_url": category_slug,
     }
--- main/views.py
+++ main/views.py
@@ -5,13 +5,7 @@ from goods.models import Categories
 
 
 def index(request):
-
-    
-    context = {
-        "title": "Home - Главная",
-        'content': "Магазин мебели HOME",
-    }
-    return render(request, "main/index.html", context)
+    return render(request, "main/index.html")
     
 
 def about(request):
